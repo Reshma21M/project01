052f70b5cff19d87ee2a85b9bc596f40f78d407c
Reshma <reshmamohamed2023@gmail.com>
2025-07-29 16:54:21 +0530

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <div>
     <h1>This is the home page</h1>
+    <h1>hello world Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius illum qui, dignissimos voluptas necessitatibus ipsa quasi, commodi unde assumenda, voluptate quos sed repellat accusantium. Adipisci quisquam est nulla eaque explicabo?</h1>
+
     </div>
   )
 }
